98024c1655673b193a34e191d1cdaf4f85fb566d
Sent fan Wyaerda <git@sent.wyaerda.org>
2015-02-15 11:45:31 +0100

    the egg of the Phoenix

---
+++ Phoenix.php
@@ -0,0 +1,42 @@
+<?php
+class Phoenix {
+	var $settings = array();
+	var $src; /* http://www.github.com/ */
+	var $root; /* /www/module/ */
+
+	function Phoenix($root=NULL, $src=FALSE){
+		if($root === NULL){ $root = dirname(__FILE__).'/'; }
+		$this->root = $root;
+		$this->load_settings();
+		if($src === FALSE && isset($this->settings['src'])){ $this->src = $this->settings['src']; } else { $this->src = $src; }
+	}
+	
+	function get_root(){ return (isset($this) ? $this->root : NULL); }
+	function get_fileshort(){ return 'phoenix.json'; }
+
+	function load_settings($file=NULL){
+		if($file === NULL){ $file = Phoenix::get_root().Phoenix::get_fileshort(); }
+		if(!file_exists($file)){ return FALSE; }
+		$this->settings = json_decode(file_get_contents($file), TRUE);
+	}
+	function save_settings($file=NULL){
+		if($file === NULL){ $file = Phoenix::get_root().Phoenix::get_fileshort(); }
+		file_put_contens($file, json_encode($this->settings));
+	}
+	
+	function backup(){ return $backup; }
+	function restore($backup){}
+	function cleanup($keep=0){ /*removes all backups except it keeps the last/most-recent $keep backups */ }
+	
+	function update($save_settings=FALSE){
+		/* gets $this->src (download, unpack) and replaces $this->src */
+		if($save_settings !== FALSE){ $this->save_settings(); }
+	}
+}
+
+//testing:
+print '<pre>';
+$Ph = new Phoenix();
+print_r($Ph);
+print '</pre>';
+?>
---
+++ README.md
@@ -0,0 +1,3 @@
+# Phoenix
+*An update helper*
+"a phoenix or phenix (Greek: φοῖνιξ phoinix) is a long-lived bird that is cyclically regenerated or reborn."
